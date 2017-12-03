51487c013aa0038240a6c262ca37cd1e95dcd8d3
Logan V <logan2211@gmail.com>
2017-12-02 23:33:19 -0600

    Restore application updateExisting for <3.4 versions
    configuration.import() calls prior to Zabbix 3.4 should retain the
    same behavior including updateExisting on "applications".
    
    The updateExisting parameter on applications was removed in 3.4.

--- lib/ansible/modules/monitoring/zabbix/zabbix_template.py
+++ lib/ansible/modules/monitoring/zabbix/zabbix_template.py
@@ -203,6 +203,7 @@ template_json:
 '''
 
 
+from distutils.version import LooseVersion
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_native
 import json
@@ -367,54 +368,62 @@ class Template(object):
         if template_name != parsed_template_json['zabbix_export']['templates'][0]['template']:
             self._module.fail_json(msg='JSON template name does not match presented name')
 
+        update_rules = {
+            'applications': {
+                'createMissing': True,
+                'updateExisting': True,
+                'deleteMissing': True
+            },
+            'discoveryRules': {
+                'createMissing': True,
+                'updateExisting': True,
+                'deleteMissing': True
+            },
+            'graphs': {
+                'createMissing': True,
+                'updateExisting': True,
+                'deleteMissing': True
+            },
+            'httptests': {
+                'createMissing': True,
+                'updateExisting': True,
+                'deleteMissing': True
+            },
+            'items': {
+                'createMissing': True,
+                'updateExisting': True,
+                'deleteMissing': True
+            },
+            'templates': {
+                'createMissing': True,
+                'updateExisting': True
+            },
+            'templateScreens': {
+                'createMissing': True,
+                'updateExisting': True,
+                'deleteMissing': True
+            },
+            'triggers': {
+                'createMissing': True,
+                'updateExisting': True,
+                'deleteMissing': True
+            },
+            'valueMaps': {
+                'createMissing': True,
+                'updateExisting': True
+            }
+        }
+
         try:
+            api_version = self._zapi.api_version()
+            # NOTE(logan): Applications updateExisting parameter was removed in 3.4
+            if LooseVersion(api_version) >= LooseVersion('3.4'):
+                del update_rules['applications']['updateExisting']
+
             self._zapi.configuration.import_({
                 'format': 'json',
                 'source': template_json,
-                'rules': {
-                    'applications': {
-                        'createMissing': True,
-                        'deleteMissing': True
-                    },
-                    'discoveryRules': {
-                        'createMissing': True,
-                        'updateExisting': True,
-                        'deleteMissing': True
-                    },
-                    'graphs': {
-                        'createMissing': True,
-                        'updateExisting': True,
-                        'deleteMissing': True
-                    },
-                    'httptests': {
-                        'createMissing': True,
-                        'updateExisting': True,
-                        'deleteMissing': True
-                    },
-                    'items': {
-                        'createMissing': True,
-                        'updateExisting': True,
-                        'deleteMissing': True
-                    },
-                    'templates': {
-                        'createMissing': True,
-                        'updateExisting': True
-                    },
-                    'templateScreens': {
-                        'createMissing': True,
-                        'updateExisting': True,
-                        'deleteMissing': True
-                    },
-                    'triggers': {
-                        'createMissing': True,
-                        'updateExisting': True,
-                        'deleteMissing': True
-                    },
-                    'valueMaps': {
-                        'createMissing': True,
-                        'updateExisting': True
-                    }
-                }
+                'rules': update_rules
             })
         except ZabbixAPIException as e:
             self._module.fail_json(
